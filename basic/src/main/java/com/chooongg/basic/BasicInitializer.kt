750fd0b3ffb7cfb12d97ee8e6522801e2d64d369
Chooongg <chooongg@outlook.com>
2022-08-24 11:56:17 +0800

    初始化添加日志

--- basic/src/main/java/com/chooongg/basic/BasicInitializer.kt
+++ basic/src/main/java/com/chooongg/basic/BasicInitializer.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.content.Context
 import androidx.annotation.Keep
 import androidx.startup.Initializer
+import com.chooongg.basic.ext.logDClass
 import com.chooongg.basic.ext.setNightMode
 import com.facebook.stetho.Stetho
 import com.tencent.mmkv.MMKV
@@ -15,6 +16,7 @@ class BasicInitializer : Initializer<Unit> {
         ApplicationManager.initialize(context as Application)
         MMKV.initialize(context)
         setNightMode(LearnMMKV.DayNightMode.get())
+        logDClass(BasicInitializer::class.java, "created")
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> = emptyList()
--- core/src/main/java/com/chooongg/core/CoreInitializer.kt
+++ core/src/main/java/com/chooongg/core/CoreInitializer.kt
@@ -3,6 +3,7 @@ package com.chooongg.core
 import android.content.Context
 import androidx.annotation.Keep
 import androidx.startup.Initializer
+import com.chooongg.basic.ext.logDClass
 import com.chooongg.core.permission.PermissionInterceptor
 import com.hjq.permissions.XXPermissions
 
@@ -10,6 +11,7 @@ import com.hjq.permissions.XXPermissions
 class CoreInitializer : Initializer<Unit> {
     override fun create(context: Context) {
         XXPermissions.setInterceptor(PermissionInterceptor())
+        logDClass(CoreInitializer::class.java, "created")
     }
 
     override fun dependencies(): MutableList<Class<out Initializer<*>>> = mutableListOf()
