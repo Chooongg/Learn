58d829dd4249f65c9e38aef4d1c708103237fec8
Chooongg <chooongg@outlook.com>
2022-10-19 16:12:54 +0800

    修改状态布局

--- extend-stateLayout/src/main/java/com/chooongg/stateLayout/state/AbstractBindingStatus.kt
+++ extend-stateLayout/src/main/java/com/chooongg/stateLayout/state/AbstractBindingStatus.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import androidx.viewbinding.ViewBinding
+import com.chooongg.basic.ext.getTClass
 import java.lang.reflect.InvocationTargetException
 import java.lang.reflect.ParameterizedType
 
@@ -35,32 +36,8 @@ abstract class AbstractBindingStatus<BINDING : ViewBinding> : AbstractState() {
 
     @Suppress("UNCHECKED_CAST")
     fun <BINDING : ViewBinding> getBindingT(context: Context): BINDING {
-        val clazz = viewModelClass<BINDING>(this)
+        val clazz = javaClass.getTClass(0) as Class<BINDING>
         val method = clazz.getMethod("inflate", LayoutInflater::class.java)
         return method.invoke(null, LayoutInflater.from(context)) as BINDING
-    }
-
-    @Suppress("UNCHECKED_CAST")
-    private fun <BINDING : ViewBinding> viewModelClass(any: Any): Class<BINDING> {
-        var genericSuperclass = any.javaClass.genericSuperclass
-        var superclass = any.javaClass.superclass
-        while (superclass != null) {
-            if (genericSuperclass is ParameterizedType) {
-                genericSuperclass.actualTypeArguments.forEach {
-                    try {
-                        return it as Class<BINDING>
-                    } catch (e: NoSuchMethodException) {
-                        e.printStackTrace()
-                    } catch (e: ClassCastException) {
-                        e.printStackTrace()
-                    } catch (e: InvocationTargetException) {
-                        e.printStackTrace()
-                    }
-                }
-            }
-            genericSuperclass = superclass.genericSuperclass
-            superclass = superclass.superclass
-        }
-        throw NullPointerException("没有找到ViewBinding泛型")
     }
 }
